e093ddefa23cfbb819badddd46d9d9dc5b9f45e4
ToniCabanas <acabascal@gmail.com>
2019-04-11 23:25:52 +0200

    Block videos and imgs - Remove block content by user - Add block content by media

--- js/main.js
+++ js/main.js
@@ -1,22 +1,40 @@
-const userNameToBlock = 'User Name'
-
 function nodeInsertedCallback(event) {
-    let elements = document.getElementsByClassName('_2a1Yw')
+    let elements = event.relatedNode.getElementsByClassName('message-in')
     Array.from(elements).map(element => modifyElement(element));
 }
 
 function modifyElement(element) {
-    if (element.innerText === userNameToBlock) {
-        getRootElement(element).className = 'hiddenElement';
+    if (element.getAttribute("blocked") === "true")
+        return;
+    element.setAttribute("blocked", "true");
+    if (element) {
+        if (isVideo(element) || isImage(element)) {
+            element.parentNode.appendChild(createButtonShowHideWidget(element));
+        }
     }
 }
 
-function getRootElement(element) {
-    console.log(element.className);
-    if (element.className.includes('message-in')) {
-        return element;
-    }
-    return getRootElement(element.parentNode);
+
+function createButtonShowHideWidget(element) {
+    var button = document.createElement('button');
+    button.innerHTML = 'Show';
+    button.className = 'buttonHide';
+    element.style.display = 'none';
+    button.addEventListener('click', event => {
+        element.style.display = element.style.display === 'none' ? '' : 'none';
+        button.innerHTML = button.innerHTML === 'Hide' ? 'Show' : 'Hide';
+    });
+    return button;
+}
+
+function isVideo(element) {
+    return element.getElementsByClassName('video-thumb').length > 0;
+}
+
+function isImage(element) {
+    let imgs = element.getElementsByTagName('img');
+    
+    return Array.from(imgs).some(img => {console.log(img); return !img.className.includes('selectable-text'); });
 }
 
 document.addEventListener('DOMNodeInserted', nodeInsertedCallback);
