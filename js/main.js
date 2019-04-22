10dd8e1c3d1268d35b83e123d8e676fff3f694db
ToniCabanas <acabascal@gmail.com>
2019-04-22 23:18:30 +0200

    Refactor to use Media objects
    - With this commit only video media is hidden.
    - Modify isImage method and getBodyElement in Image class to allow works
      wiht this kind of media.

--- js/main.js
+++ js/main.js
@@ -8,33 +8,85 @@ function modifyElement(element) {
         return;
     element.setAttribute("blocked", "true");
     if (element) {
-        if (isVideo(element) || isImage(element)) {
-            element.parentNode.appendChild(createButtonShowHideWidget(element));
+        console.log(element);
+        let media = Media.factory(element);
+        if (media) {
+            console.log(media);
+            media.addHideButton();
+            media.hideElement();
         }
     }
 }
 
-
-function createButtonShowHideWidget(element) {
-    var button = document.createElement('button');
-    button.innerHTML = 'Show';
-    button.className = 'buttonHide';
-    element.style.display = 'none';
-    button.addEventListener('click', event => {
-        element.style.display = element.style.display === 'none' ? '' : 'none';
-        button.innerHTML = button.innerHTML === 'Hide' ? 'Show' : 'Hide';
-    });
-    return button;
-}
-
-function isVideo(element) {
-    return element.getElementsByClassName('video-thumb').length > 0;
-}
-
-function isImage(element) {
-    let imgs = element.getElementsByTagName('img');
-    
-    return Array.from(imgs).some(img => {console.log(img); return !img.className.includes('selectable-text'); });
-}
-
 document.addEventListener('DOMNodeInserted', nodeInsertedCallback);
+
+class Media {
+
+    constructor(element) {
+        this.element = element;
+        this.bodyElement = null;
+    }
+
+    static factory(msgElement) {
+        switch (true) {
+            case Image.isImage(msgElement):
+                return new Image(msgElement);
+            case Video.isVideo(msgElement):
+                return new Video(msgElement);
+            default:
+                return null;
+        }
+    }
+
+    hideElement() {
+        this.getBodyElement().style.display = 'none';
+    }
+
+    addHideButton() {
+        this.getBodyElement().parentNode.appendChild(this.createButtonShowHideWidget());
+    }
+
+    createButtonShowHideWidget() {
+        var button = document.createElement('button');
+        button.innerHTML = 'Show';
+        button.className = 'buttonHide';
+        button.addEventListener('click', event => {
+            this.getBodyElement().style.display = this.getBodyElement().style.display === 'none' ? '' : 'none';
+            button.innerHTML = button.innerHTML === 'Hide' ? 'Show' : 'Hide';
+        });
+        return button;
+    }
+}
+
+class Image extends Media {
+
+    getBodyElement() {
+        if (!this.bodyElement) {
+            let elements = this.element.getElementsByClassName('_3v3PK');
+            this.bodyElement = elements.length > 0 ? elements[0] : null;
+        }
+        return this.bodyElement;
+    }
+
+    static isImage(msgElement) {
+        return false;
+        let imgs = msgElement.getElementsByTagName('img');
+        return Array.from(imgs).some(img => { return !img.className.includes('selectable-text'); });
+    }
+
+}
+
+class Video extends Media {
+
+    getBodyElement() {
+        if (!this.bodyElement) {
+            let elements = this.element.getElementsByClassName('video-thumb');
+            this.bodyElement = elements.length > 0 ? elements[0] : null;
+        }
+        return this.bodyElement;
+    }
+
+    static isVideo(msgElement) {
+        return msgElement.getElementsByClassName('video-thumb').length > 0;
+    }
+}
